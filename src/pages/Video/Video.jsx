32389392c8509b68868074be49b914760a8c1ff1
Akshay Umredkar <94985332+Akshay0798@users.noreply.github.com>
2024-02-26 16:00:52 +0530

    commit changes

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -9,7 +9,7 @@ const Video = () => {
   return (
     <div className='play-container'>
       <PlayVideo videoId={videoId} />
-      <Recommended/>
+      <Recommended categoryId={categoryId}/>
     </div>
   )
 }
